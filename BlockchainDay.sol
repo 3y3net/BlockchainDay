4e9520dafa2cfee6397896cf77c25a8c7b229bae
test <test>
2022-07-13 22:24:29 +0200

    final upgrade

--- BlockchainDay.sol
+++ BlockchainDay.sol
@@ -1,3 +1,5 @@
+0x710F8bC86dB7E72e81DE4FA71588A7Bd7aCe37eb
+
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -20,7 +22,7 @@ contract BlockchainDay is ERC1155Supply, AccessControlEnumerable, Ownable {
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
 
-    constructor() ERC1155("http://nft.myownsatoshi.com/bday/{id}.json") {
+    constructor() ERC1155("http://nft.myownsatoshi.com/data/bday/{id}.json") {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(MINTER_ROLE, _msgSender());
         _setupRole(BURNER_ROLE, _msgSender());
@@ -43,16 +45,31 @@ contract BlockchainDay is ERC1155Supply, AccessControlEnumerable, Ownable {
      //May receive crypto... well who knows
     receive() external payable {}
 
+    function uri(uint256 tokenId) public view virtual override returns (string memory) {
+        return string(
+            abi.encodePacked(
+            string(abi.encodePacked("http://nft.myownsatoshi.com/data/bday/",Strings.toString(tokenId))),
+            ".json")
+            
+            );
+    }
+
+    //Royalties Address
+    function setRoyaltiesAddress(address payable rAddress) public onlyOwner {
+        _royaltiesAddress=rAddress;
+    }
+
+    //Royalties fee
+    function setRoyaltiesBasicPoints(uint96 rBasicPoints) public onlyOwner {
+        _royaltiesBasicPoints=rBasicPoints;
+    }
+
     //The SC is not desgined to receive funds but it's better to have this funtion... believe me
     function withdraw(uint amount) external {
         require(hasRole(WITHDRAW_ROLE, _msgSender()), "Exception: must have withdraw role to get crypto");
         payable(msg.sender).transfer(amount);
     }
-
-    function uri(uint256 tokenId) public view virtual override returns (string memory) {
-        return string(abi.encodePacked("http://nft.myownsatoshi.com/bday/",Strings.toString(tokenId)));
-    }
-
+    
     function royaltyInfo(uint256 _tokenId, uint256 _salePrice ) external view returns ( address receiver, uint256 royaltyAmount) {
         if(exists(_tokenId))
             return(_royaltiesAddress, (_salePrice * _royaltiesBasicPoints)/10000);        
